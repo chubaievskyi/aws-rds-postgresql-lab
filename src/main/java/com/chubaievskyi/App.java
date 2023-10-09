cf9d59d6074566c837f901a237379482524101a7
Pavlo Chubaievskyi <chubaievskyi@gmail.com>
2023-10-09 21:56:17 +0300

    Add some features

---
+++ src/main/java/com/chubaievskyi/App.java
@@ -0,0 +1,17 @@
+package com.chubaievskyi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        LOGGER.info("Program start!");
+
+
+
+        LOGGER.info("End of program!");
+    }
+}
